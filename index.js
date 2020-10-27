fe01f070b2274732f58668718af6891f69d244af
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-10-27 12:38:16 +0000

    Add Prism highlighting

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-const defaultOptions = require('./components/options-default.js');
+const posthtml = require('posthtml');
+const prism = require('posthtml-prism');
 
-module.exports = function(eleventyConfig, customOptions) {
-	eleventyConfig.addTransform('syntaxHighlighter', function(input, ...instanceOptions) {
-		return foo();
+module.exports = function(eleventyConfig, options) {
+	eleventyConfig.addTransform('syntaxHighlighter', function(code, outputPath) {
+		if(outputPath.endsWith('.html')) {
+			return highlight(code);
+		}
+		else {
+			return code;
+		}
 	});
 }
+
+function highlight(code) {
+	return posthtml([prism()])
+  .process(code)
+  .then(result => result.html)
+}
+
+
+
+/*
+TODO:
+	load additional languages
+	add plugins manually?
+		show line numbers
+		highlight specific line numbers
+	auto add css
+*/
