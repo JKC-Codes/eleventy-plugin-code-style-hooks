fec211b9a34cbd64e9bc68a93c9f2980e5152576
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-11-03 12:57:58 +0000

    Simplify language name and minimise exports

--- components/regular-expressions.js
+++ components/regular-expressions.js
@@ -1,18 +1,12 @@
 // Regex = start of line or whitespace + 'lang' + optional 'uage' + '-'
 const languageClassStart = String.raw`(?:^|\s)lang(?:uage)?-`;
 
-// Regex = a letter + 0 or more letters or numbers + 0 or more patterns of: ('-' + a letter + 0 or more letters or numbers)
-const languageName = String.raw`[a-z][a-z\d]*(?:-[a-z][a-z\d]*)*`;
+// Regex = 1 or more letters, numbers or underscores + end of line or whitespace
+const languageName = String.raw`([\w-]+)(?:$|\s)`;
 
 const languageClass = String.raw`${languageClassStart}${languageName}`;
 
 
 module.exports = {
-	class: {
-		language: {
-			className: languageClass,
-			languageName: languageName,
-			identifier: languageClass
-		}
-	}
+	class: languageClass
 }
