09351e43aa543ee41ba0a89fe1eaa8b59c5bf35b
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-11-03 14:20:20 +0000

    Fix languages not being detected when multiple

--- components/regular-expressions.js
+++ components/regular-expressions.js
@@ -1,8 +1,8 @@
 // Regex = start of line or whitespace + 'lang' + optional 'uage' + '-'
-const languageClassStart = String.raw`(?:^|\s)lang(?:uage)?-`;
+const languageClassStart = String.raw`(?<=^|\s)lang(?:uage)?-`;
 
 // Regex = 1 or more letters, numbers or underscores + end of line or whitespace
-const languageName = String.raw`([\w-]+)(?:$|\s)`;
+const languageName = String.raw`([\w-]+)(?=$|\s)`;
 
 const languageClass = String.raw`${languageClassStart}${languageName}`;
 
