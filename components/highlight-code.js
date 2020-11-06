386bc32dcc4946ab853ec3c337bd4a6edfa53b6f
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-11-06 13:49:27 +0000

    Add code highlighting

--- components/highlight-code.js
+++ components/highlight-code.js
@@ -1,108 +1,37 @@
-/*
-	If language-none class on code: don't highlight
-	Run prism on code
-*/
+// TODO: option to need escaped HTML or not
+// TODO: auto detect language?
 
-const regEx = require('./regular-expressions.js');
+const parseHTML = require('posthtml-render');
 const Prism = require('prismjs');
-const loadLanguages = require('prismjs/components/');
+const loadLanguage = require('prismjs/components/');
+const regEx = require('./regular-expressions.js');
 
-module.exports = function(AST, codeElements) {
-	// AST = Abstract Syntax Tree from HTML Parser
+module.exports = function(codeElements) {
+	codeElements.forEach(codeElement => {
+		const language = getLanguage(codeElement);
 
-	highlightCode(AST, codeElements);
+		// Only highlight code blocks with a language class
+		if(language) {
+			if(!Prism.languages[language]) {
+				// Prism only loads markup, css, clike and javascript by default
+				loadLanguage.silent = true;
+				loadLanguage([language]);
+			}
 
-	return AST;
+			// Skip highlighting unrecognised languages, including 'language-none'
+			if(Prism.languages[language]) {
+				codeElement.content.forEach((node, index) => {
+					codeElement.content[index] = Prism.highlight(parseHTML(node), Prism.languages[language], language);
+				})
+			}
+		}
+	})
 }
 
-function highlightCode(AST, codeElements) {
-	return AST;
+function getLanguage(element) {
+	if(element.attrs && element.attrs.class) {
+		const classMatch = element.attrs.class.match(new RegExp(regEx.classLanguage));
+		// Array index 1 is the language capture group
+		return classMatch ? classMatch[1] : undefined;
+	}
 }
-
-
-
-
-// const Prism = require('prismjs');
-// const render = require('posthtml-render');
-// const loadLanguages = require('prismjs/components/');
-
-// function createPrismPlugin(options) {
-//   return function (tree) {
-//     const highlightCodeTags = node => tree.match.call(node, {tag: 'code'}, highlightNode);
-
-//     if (options.inline) {
-//       highlightCodeTags(tree);
-//     } else {
-//       tree.match({tag: 'pre'}, highlightCodeTags);
-//     }
-//   };
-// }
-
-// function highlightNode(node) {
-//   const attrs = node.attrs || {};
-//   const classList = `${attrs.class || ''}`.trimStart();
-
-//   if ('prism-ignore' in attrs) {
-//     delete node.attrs['prism-ignore'];
-//     return node;
-//   }
-
-//   if (classList.includes('prism-ignore')) {
-//     node.attrs.class = node.attrs.class.replace('prism-ignore', '').trim();
-//     return node;
-//   }
-
-//   const lang = getExplicitLanguage(classList);
-
-//   if (lang && !classList.includes(`language-${lang}`)) {
-//     attrs.class = `${classList || ''} language-${lang}`.trimStart();
-//   }
-
-//   node.attrs = attrs;
-
-//   if (node.content) {
-//     let html = '';
-
-//     if (node.content[0].tag && !node.content[0].content) {
-//       html = `<${node.content[0].tag}>`;
-//     } else {
-//       html = render(node.content);
-//     }
-
-//     node.content = mapStringOrNode(html, lang);
-//   }
-
-//   return node;
-// }
-
-// function mapStringOrNode(stringOrNode, lang = null) {
-//   if (typeof stringOrNode === 'string') {
-//     if (lang) {
-//       if (!Object.keys(Prism.languages).includes(lang)) {
-//         loadLanguages.silent = true;
-//         loadLanguages([lang]);
-//       }
-
-//       return Prism.highlight(stringOrNode, Prism.languages[lang], lang);
-//     }
-
-//     return Prism.highlight(stringOrNode, Prism.languages.markup, 'markup');
-//   }
-
-//   highlightNode(stringOrNode);
-//   return stringOrNode;
-// }
-
-// function getExplicitLanguage(classList) {
-//   const matches = classList.match(/(?:lang|language)-(\w*)/);
-//   return matches === null ? null : matches[1];
-// }
-
-// module.exports = options => {
-//   options = options || {};
-//   options.inline = options.inline || false;
-
-//   return function (tree) {
-//     return createPrismPlugin(options)(tree);
-//   };
-// };
