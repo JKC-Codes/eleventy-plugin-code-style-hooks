733530cdcbb9d6311530060897d1319718a5bf1a
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-11-06 13:57:17 +0000

    Add CSS injection

--- components/add-CSS.js
+++ components/add-CSS.js
@@ -1,4 +1,28 @@
-// ast = Abstract Syntax Tree from HTML Parser
+// TODO: pass in CSS option
+
+const regEx = require('./regular-expressions.js');
+
 module.exports = function(AST, codeElements) {
-	return AST;
+	// AST = Abstract Syntax Tree from HTML Parser
+	const needsCSS = codeElements.some(node => {
+		return node.attrs && new RegExp(regEx.classLanguage).test(node.attrs.class);
+	});
+
+	if(needsCSS) {
+		AST.match({tag: 'head'}, head => {
+			if(!head.content) {
+				head.content = [];
+			}
+
+			head.content.push({
+				tag: 'link',
+				attrs: {
+					rel: 'stylesheet',
+					href: 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.22.0/themes/prism-coy.min.css'
+				},
+			});
+
+			return head;
+		});
+	}
 }
