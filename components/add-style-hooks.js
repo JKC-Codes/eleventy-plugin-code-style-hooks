cf42a08645f025b883948caddfbf8451705d368d
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-11-20 12:39:59 +0000

    Fix invalid language adding first line number

--- components/add-style-hooks.js
+++ components/add-style-hooks.js
@@ -2,15 +2,10 @@ const Prism = require('prismjs');
 const loadLanguage = require('prismjs/components/');
 const regEx = require('./regular-expressions.js');
 
-module.exports = function(codeElements, options) {
+module.exports = function(codeElements) {
 	codeElements.forEach(codeElement => {
-		highlightSyntax(codeElement, options);
-	})
-}
-
-function highlightSyntax(node, options) {
-	// Array index 1 is the language capture group
-	const language = node.attrs.class.match(new RegExp(regEx.classLanguage))[1];
+			// Array index 1 is the language capture group
+	const language = codeElement.attrs.class.match(new RegExp(regEx.classLanguage))[1];
 
 	// Prism only loads markup, css, clike and javascript by default
 	if(!Prism.languages[language]) {
@@ -20,15 +15,24 @@ function highlightSyntax(node, options) {
 
 	// Skip highlighting unrecognised languages including 'language-none'
 	if(Prism.languages[language]) {
-		highlightNode(node, language);
+		highlightNode(codeElement, language);
 	}
+	})
 }
 
 function highlightNode(node, language) {
 	if(node.content) {
 		node.content.forEach((contentItem, index) => {
 			if(typeof contentItem === 'string') {
-				node.content[index] = Prism.highlight(contentItem, Prism.languages[language], language);
+				let highlightedString = Prism.highlight(contentItem, Prism.languages[language], language);
+
+				// Add line break style hooks. Regex = optional return + new line
+				highlightedString = highlightedString.replace(/((?:\r)?\n)/g, '<span class="token line-break" aria-hidden="true">$&</span>');
+
+				// Add style hook for first line number
+				highlightedString = '<span class="token start-of-code" aria-hidden="true"></span>' + highlightedString;
+
+				node.content[index] = highlightedString;
 			}
 			// Prevent Code within Code highlighting tokens
 			else if(contentItem.tag === 'code') {
