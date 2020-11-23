2fc05ecdf0a940be8b1c8866ca801d71258e051d
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-11-23 16:36:57 +0000

    Fix empty language class

--- tests/test-site/pages/javascript.11ty.js
+++ tests/test-site/pages/javascript.11ty.js
@@ -8,8 +8,10 @@ class page {
 	render({tests}) {
 		return tests.reduce((acc, cur) => {
 			const code = cur.code.replace(/</gi, '&lt;');
+			const language = cur.language ? ` class="language-${cur.language}"` : '';
+
 			return acc +
-`<pre><code class="language-${cur.language}">${code}
+`<pre><code${language}>${code}
 </code></pre>
 `;
 		}, '');
