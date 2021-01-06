6808b27a58e06f9994096096fc938d1214789b3a
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2021-01-06 14:57:52 +0000

    Remove unnecessary comment

--- components/add-color-hooks.js
+++ components/add-color-hooks.js
@@ -1,7 +1,6 @@
 const regEx = require('./regular-expressions.js');
 
 module.exports = function(content, index) {
-	// const colorsRegEx = new RegExp(regEx.color, 'gi');
 	const colorsRegEx = new RegExp(regEx.color, 'gi');
 	// $& = the matched colour
 	const colorPreview = '<span class="token color-value" style="--color-value: $&" aria-hidden="true"></span>$&';
