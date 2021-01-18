d1589dbce5e88e73a7a9d726330143fbc2c3893a
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2021-01-18 13:35:13 +0000

    Add named colours

--- components/regular-expressions.js
+++ components/regular-expressions.js
@@ -10,14 +10,11 @@ const languageStart = String.raw`lang(?:uage)?`;
 // Regex = 1 or more letters, numbers, underscores or dashes in a capture group
 const languageName = String.raw`([\w-]+)`;
 
-// Regex = optional return + new line
-const lineNew = String.raw`((?:\r)?\n)`;
+// Regex = look behind to check for whitespace or ',' or start of line or '(' or ':' in a non-capture group
+const colorStart = String.raw`(?<=\s|,|^|\(|:)`;
 
-// Regex = look behind to check for start of line or whitespace or '>' or ':' in a non-capture group
-const colorStart = String.raw`(?<=^|\s|>|:)`;
-
-// Regex = look ahead to check for end of line or whitespace or '<' or ';' in a non-capture group
-const colorEnd = String.raw`(?=$|\s|<|;)`;
+// Regex = look ahead to check for whitespace or ',' or end of line or ')' or ';' in a non-capture group
+const colorEnd = String.raw`(?=\s|,|$|\)|;)`;
 
 // Regex = 1 or more whitespace characters or a comma surrounded by optional whitespace in a non-capture group
 const colorSeparator = String.raw`(?:\s+|(?:\s*,\s*))`;
@@ -55,6 +52,12 @@ const colorLab = String.raw`lab\(\s*${colorPercentage}\s+${colorNumber}\s+${colo
 // Regex = 'lch(' + optional whitespace + precentage regex + one or more whitespace + number regex + one or more whitespace + hue regex + optional whitespace followed by '/' + optional whitespace + alpha regex + optional whitespace + ')'
 const colorLCH = String.raw`lch\(\s*${colorPercentage}\s+${colorNumber}\s+${colorHue}(?:\s*\/\s*${colorAlpha})?\s*\)`;
 
+// Regex = All CSS named colours in a non-capture group
+const colorNamed = String.raw`(?:aliceblue|antiquewhite|aqua|aquamarine|azure|beige|bisque|black|blanchedalmond|blue|blueviolet|brown|burlywood|cadetblue|chartreuse|chocolate|coral|cornflowerblue|cornsilk|crimson|cyan|darkblue|darkcyan|darkgoldenrod|darkgray|darkgreen|darkgrey|darkkhaki|darkmagenta|darkolivegreen|darkorange|darkorchid|darkred|darksalmon|darkseagreen|darkslateblue|darkslategray|darkslategrey|darkturquoise|darkviolet|deeppink|deepskyblue|dimgray|dimgrey|dodgerblue|firebrick|floralwhite|forestgreen|fuchsia|gainsboro|ghostwhite|gold|goldenrod|gray|green|greenyellow|grey|honeydew|hotpink|indianred|indigo|ivory|khaki|lavender|lavenderblush|lawngreen|lemonchiffon|lightblue|lightcoral|lightcyan|lightgoldenrodyellow|lightgray|lightgreen|lightgrey|lightpink|lightsalmon|lightseagreen|lightskyblue|lightslategray|lightslategrey|lightsteelblue|lightyellow|lime|limegreen|linen|magenta|maroon|mediumaquamarine|mediumblue|mediumorchid|mediumpurple|mediumseagreen|mediumslateblue|mediumspringgreen|mediumturquoise|mediumvioletred|midnightblue|mintcream|mistyrose|moccasin|navajowhite|navy|oldlace|olive|olivedrab|orange|orangered|orchid|palegoldenrod|palegreen|paleturquoise|palevioletred|papayawhip|peachpuff|peru|pink|plum|powderblue|purple|rebeccapurple|red|rosybrown|royalblue|saddlebrown|salmon|sandybrown|seagreen|seashell|sienna|silver|skyblue|slateblue|slategray|slategrey|snow|springgreen|steelblue|tan|teal|thistle|tomato|turquoise|violet|wheat|white|whitesmoke|yellow|yellowgreen)`;
+
+// Regex = '<' + optional '/' + 1 or more characters that aren't '>' or whitespace + optional any number of space followed by 0 or more characters that are not '>' + '>'
+const HTMLTag = String.raw`<\/?(?:[^>\s]+)(?:\s[^>]*)?>`;
+
 
 module.exports = {
 	wordStart,
@@ -66,7 +69,10 @@ module.exports = {
 	// 'data-line-numbers' or 'data-show-language' or 'data-highlight-syntax' or 'data-show-color'
 	attributeData: String.raw`^data-(line-numbers|show-language|highlight-syntax|show-color)$`,
 	// new line
-	lineNew,
+	lineNew: String.raw`\n`,
 	// hex, RGB, HSL, Lab or LCH colour in a capture group
-	color: String.raw`${colorStart}((?:${colorHex})|(?:${colorRGB})|(?:${colorHSL})|(?:${colorHWB})|(?:${colorLab})|(?:${colorLCH}))${colorEnd}`
+	color: String.raw`${colorStart}((?:${colorHex})|(?:${colorRGB})|(?:${colorHSL})|(?:${colorHWB})|(?:${colorLab})|(?:${colorLCH}))${colorEnd}`,
+	colorNamed,
+	number: colorNumber,
+	HTMLTag
 }
