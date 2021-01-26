2bb56a5e290d8f3e7d4ecaddbc2742bcff9fdb37
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2021-01-26 13:37:29 +0000

    Fix first line numbers not being in syntax tree

--- components/add-first-line-numbers.js
+++ components/add-first-line-numbers.js
@@ -1,16 +1,15 @@
 const regEx = require('./regular-expressions.js');
 
 module.exports = function addFirstLineNumber(lastNewLineObject) {
+	const lineNumber = {
+		tag: 'span',
+		attrs: {
+			class: 'token line-number',
+			'aria-hidden': 'true'
+		}
+	};
 
 	if(lastNewLineObject.node.tag === 'pre' && lastNewLineObject.index === null) {
-		const lineNumber = {
-			tag: 'span',
-			attrs: {
-				class: 'token line-number',
-				'aria-hidden': 'true'
-			}
-		};
-
 		lastNewLineObject.node.content.unshift(lineNumber);
 		// Prevent infinite loops
 		lastNewLineObject.state.index++;
@@ -19,11 +18,15 @@ module.exports = function addFirstLineNumber(lastNewLineObject) {
 		const content = lastNewLineObject.node.content;
 		const index = lastNewLineObject.index;
 		// Regex = positive lookahead for any non-line-break characters from end of string
-		const lastNewLine = new RegExp(`${regEx.lineNew}(?=.*$)`);
-		const lineNumber = '<span class="token line-number" aria-hidden="true"></span>';
-		const replacementString = content[index].replace(lastNewLine, `$&${lineNumber}`);
+		const lastNewLine = new RegExp(`${regEx.lineNew}(?=.*$)`, 'g');
+		lastNewLine.exec(content[index]);
+		const start = content[index].slice(0, lastNewLine.lastIndex);
+		const end = content[index].slice(lastNewLine.lastIndex);
+		const replacementNodes = [start, lineNumber, end];
 
-		content.splice(index, 1, replacementString);
+		content.splice(index, 1, ...replacementNodes);
+		// Prevent infinite loops
+		lastNewLineObject.state.index += replacementNodes.length - 1;
 	}
 
 	lastNewLineObject.node = {tag: 'code'}
