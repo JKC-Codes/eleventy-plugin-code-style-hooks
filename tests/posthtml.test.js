a91b75ea258a14049ce055410e3068d5485af8fc
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2021-01-29 12:11:43 +0000

    Add PostHTML tests

---
+++ tests/posthtml.test.js
@@ -0,0 +1,131 @@
+const test = require('ava');
+const PostHTML = require('posthtml');
+const { posthtml, parser } = require('../index.js');
+
+const defaultOptions = {
+	defaultLanguage: null,
+	highlightSyntax: true,
+	markdownTrimTrailingNewline: true,
+	removeRedundancy: true,
+	showColors: true,
+	showLanguages: true,
+	showLineNumbers: true,
+	scripts: [],
+	styles: []
+};
+
+
+function addCodeHooks(HTMLString, options) {
+	return PostHTML([posthtml(options)])
+		.process(HTMLString)
+		.then(result => result.html);
+}
+
+
+test('Can be used directly with PostHTML', async t => {
+	return addCodeHooks('<code class="language-js"></code>')
+	.then(result => {
+		t.is(result, '<code class="language-js" data-language="js"></code>');
+	});
+});
+
+
+test('Can be used with custom options by PostHTML', async t => {
+	return addCodeHooks('<code></code>', {defaultLanguage: 'js'})
+	.then(result => {
+		t.is(result, '<code class="language-js" data-language="js"></code>');
+	});
+});
+
+
+test.only('Can combine PostHTML and options parser', async t => {
+	return addCodeHooks('<code></code>', {
+		...defaultOptions,
+		parsed: true,
+		scripts: [{
+			attrs: {
+				src: 'foo.js'
+			},
+			tag: 'script'
+		}]
+	})
+	.then(result => {
+		t.is(result, '<script src="foo.js"></script><code></code>');
+	});
+});
+
+
+test('Can use options parser separately', t => {
+	t.deepEqual(parser(), {
+		...defaultOptions,
+		parsed: true,
+		usingPostHTML: true
+	});
+
+	t.deepEqual(parser({defaultLanguage: 'foo'}), {
+		...defaultOptions,
+		parsed: true,
+		usingPostHTML: true,
+		defaultLanguage: 'foo'
+	});
+
+	t.deepEqual(parser({highlightSyntax: false}), {
+		...defaultOptions,
+		parsed: true,
+		usingPostHTML: true,
+		highlightSyntax: false
+	});
+
+	t.deepEqual(parser({removeRedundancy: false}), {
+		...defaultOptions,
+		parsed: true,
+		usingPostHTML: true,
+		removeRedundancy: false
+	});
+
+	t.deepEqual(parser({showColors: false}), {
+		...defaultOptions,
+		parsed: true,
+		usingPostHTML: true,
+		showColors: false
+	});
+
+	t.deepEqual(parser({showLanguages: false}), {
+		...defaultOptions,
+		parsed: true,
+		usingPostHTML: true,
+		showLanguages: false
+	});
+
+	t.deepEqual(parser({showLineNumbers: false}), {
+		...defaultOptions,
+		parsed: true,
+		usingPostHTML: true,
+		showLineNumbers: false
+	});
+
+	t.deepEqual(parser({scripts: 'foo.js'}), {
+		...defaultOptions,
+		parsed: true,
+		usingPostHTML: true,
+		scripts: [{
+			attrs: {
+				src: 'foo.js'
+			},
+			tag: 'script'
+		}]
+	});
+
+	t.deepEqual(parser({styles: 'foo.css'}), {
+		...defaultOptions,
+		parsed: true,
+		usingPostHTML: true,
+		styles: [{
+			attrs: {
+				href: 'foo.css',
+				rel: 'stylesheet'
+			},
+			tag: 'link'
+		}]
+	});
+});
